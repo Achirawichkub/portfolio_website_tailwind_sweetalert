75da6c9236fb10ac64bce2092800164a492a684d
GarfieldAchirawich♠️♥️ <106904757+Achirawichkub@users.noreply.github.com>
2023-06-09 13:12:31 +0700

    add file all

---
+++ Tailwind&sweetalert/main.js
@@ -0,0 +1,11 @@
+// Initialization for ES Users
+import {
+    Dropdown,
+    Collapse,
+    initTE,
+    Carousel,
+    Ripple,
+    Input,
+  } from "tw-elements";
+  
+  initTE({ Dropdown, Collapse , Carousel,Ripple,Input });
